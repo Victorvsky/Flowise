bb41ad3d9a61324ee1ac65730ab873f99587fd9d
Victor <victor.bolog@gmail.com>
2024-06-18 08:45:26 +0300

    change logo

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,5 +1,4 @@
-import logo from '@/assets/images/flowise_logo.png'
-import logoDark from '@/assets/images/flowise_logo_dark.png'
+import logo from '@/assets/images/writegpt.png'
 
 import { useSelector } from 'react-redux'
 
@@ -10,11 +9,7 @@ const Logo = () => {
 
     return (
         <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row' }}>
-            <img
-                style={{ objectFit: 'contain', height: 'auto', width: 150 }}
-                src={customization.isDarkMode ? logoDark : logo}
-                alt='Flowise'
-            />
+            <img style={{ objectFit: 'contain', height: 'auto', width: 150 }} src={logo} alt='Flowise' />
         </div>
     )
 }
